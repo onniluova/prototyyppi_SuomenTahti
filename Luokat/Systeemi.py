cb05f80b87767f5e6c12ecaa80f04fbca9d2c4e0
onniluo <onni.luova@gmail.com>
2023-09-28 12:06:20 +0300

    Systeemiin lisätty uusia funktioita.

---
+++ Luokat/Systeemi.py
@@ -0,0 +1,10 @@
+import math
+
+class rahat:
+    def __init__(self, rahaMaara = 500):
+        self.rahaMaara = rahaMaara
+
+    def tarkistaRahatilanne(self):
+        if self.rahaMaara >= self:
+            print(self.rahaMaara)
+
--- Luokat/rahaSysteemi.py
+++
@@ -1,5 +0,0 @@
-import math
-
-class rahat:
-    def __init__(self, rahaMaara = 500):
-        self.rahaMaara = rahaMaara
