9b7d8aa432b09ee585618a3cad247e636ef603f7
Ilmarielomaa <ilmari.elomaa03@gmail.com>
2023-09-29 12:55:02 +0300

    Loin tankkaus funktion, hienosäätöä vailla

--- Systeemi.py
+++ Systeemi.py
@@ -17,6 +17,24 @@ class polttoaine:
         self.polttoaineMaara = polttoaineMaara
     #Polttaine litroissa asetus, 100 on maksimi ja tankki on täynnä alussa
     #Tankkaus funktio, tankkaamisesta tulee maksaa sitten, kun et voi liikkua enää mihinkään. Tankkaaminen maksaa 100 euroa.
+
+class tankkaus:
+    def tankki_tayteen(self, rahat, polttoaine):
+        if rahat >= 100:
+            polttoaine = 100
+            rahat -= 100
+        else:
+            print("Rahat ei riitä tankin täyttämiseen. ")
+
+        return rahat, polttoaine
+
+rahat = 500
+polttoaine = 0
+
+rahat, polttoaine = tankki_tayteen(rahat, polttoaine)
+
+print("Rahaa jäljellä:", rahat)
+print("Polttoainetta tankissa:", polttoaine)
 class sijainti:
     def __init__(self, koordinaatit):
         self.koordinaatit = koordinaatit
