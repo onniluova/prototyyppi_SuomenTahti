de09c37af50a82ef3bb76de2e9c1da27a12088a8
jannassling <Jannassl@metropolia.fi>
2023-09-30 17:20:23 +0300

    SQL - kyselyä

--- satunnaisetTapahtumat.py
+++ satunnaisetTapahtumat.py
@@ -7,16 +7,40 @@
     #• Nopan luku 6-11: Ei tapahdu mitään.
     #• Nopan luku 12: Löydät paikallisen Tokmannin. Saat sieltä ämpärin. Ämpäri lisää ilmastopisteitä 100 pisteellä.
 
+
+
+
+    # SQL- kyselyt:
+    # Paikkakunnat : select municipality from airport, country
+    # where airport.iso_country = country.iso_country and country.name = "Finland"
+
 import random
 def sattuma(noppa):
 
     if noppa == 1:
         print("Lentokoneessa on vikaa. Se täytyy korjata")
+
     elif noppa ==2:
         raha_lista=[100,200,300]
         raha_maara = random.randint(0,2)
-
         print(f"Löysit {raha_lista[raha_maara]}€")
+
+    elif noppa ==3:
+        print("Myötätuuli")
+
+    elif noppa == 4:
+        print("Sinut ryöstettiin. Menetit 100€")
+
+    elif noppa == 5:
+        print("Sää on liian huono lentämiseen. Joudut yöpymään hotellissa. -100€")
+
+    elif noppa in range(6,11):
+        print("Ei tapahdu mitään")
+
+    elif noppa == 12:
+        print("ÄMPÄRIARVONTA!!")
+
+
 
 
 
@@ -24,5 +48,6 @@ def sattuma(noppa):
 
 
 noppa = random.randint(1,12)
+print(noppa)
 
 print(sattuma(noppa))
