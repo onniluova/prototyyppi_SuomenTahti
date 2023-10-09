ed32b931ab27d2e0235314340d59d47984092880
jannassling <Jannassl@metropolia.fi>
2023-10-09 10:31:29 +0300

    Main looppi

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import satunnaisetTapahtumat
 
 #Peli alkaa tästä. Pelilooppiin siirrytään kun pelaaja on valmis pelaamaan.
 
-rahaSys = Systeemi.rahat()
+#rahaSys = Systeemi.rahat()
 
 pelaajanNimi = input(("Tervetuloa pelaamaan Suomen Tähteä! Syötä nimi: "))
 print("Hei, " + pelaajanNimi)
 
-print("Rahasi: " + str(rahaSys.tarkistaRahatilanne(50)))
+#print("Rahasi: " + str(rahaSys.tarkistaRahatilanne(50)))
---
+++ sql.py
@@ -0,0 +1,3 @@
+#select airport.name,municipality, latitude_deg, longitude_deg from airport
+#where airport.iso_country in(select country.iso_country from country
+#where country.name = "Finland")
