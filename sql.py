6170846801390f1b099b1f486a8f6c80f5333142
AleksiTakala <at.345688@gmail.com>
2023-10-10 15:46:43 +0300

    geopymatka file pois

--- sql.py
+++ sql.py
@@ -1,5 +1,5 @@
 import mysql.connector
-
+from geopy import distance
 def kohteet():
     sql = "SELECT airport.name, municipality, latitude_deg, longitude_deg, airport.ident FROM airport"
     sql += " inner join country on country.iso_country = airport.iso_country where country.name =" + "'Finland'" + " AND type = 'medium_airport'"
@@ -23,3 +23,21 @@ yhteys = mysql.connector.connect(
 )
 sijainti = "EFHK"
 kohteet()
+
+def icaokoodi(idd):
+    sql = "SELECT latitude_deg, longitude_deg from airport"
+    sql += " WHERE ident = '" + idd + "'"
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    return tulos
+
+koodit = input('Anna kahden lentokentän ICAO koodi ')
+a = icaokoodi(koodit)
+koodit2 = input('Anna toinen ')
+b = icaokoodi(koodit2)
+
+print(distance.distance(a, b).km)
+
+polttoaine = distance.distance(a, b).km * 0.15
+print(polttoaine)
