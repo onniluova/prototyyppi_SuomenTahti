db7f8baf9f0743beed331d5a2e0bb4af1b989f01
jannassling <Jannassl@metropolia.fi>
2023-10-10 15:14:11 +0300

    SQL tiedostoon luoto yhteys

--- sql.py
+++ sql.py
@@ -2,8 +2,9 @@ import mysql.connector
 
 def kohteet():
     sql = "SELECT airport.name, municipality, latitude_deg, longitude_deg, airport.ident FROM airport"
-    sql += " inner join country on country.iso_country = airport.iso_country where country.name =" + "'Finland'"
+    sql += " inner join country on country.iso_country = airport.iso_country where country.name =" + "'Finland'" + " AND type = 'medium_airport'"
     sql += " order by latitude_deg"
+    sql += " LIMIT 26"
 
     kursori = yhteys.cursor()
     kursori.execute(sql)
