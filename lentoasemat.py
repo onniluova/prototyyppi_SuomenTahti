b4bdd42817a49bcaa4e29eb93886b57bf68a117b
onboni <onni.luova@gmail.com>
2023-12-04 11:00:39 +0200

    Lentoasemat.py municipality, siirrykKohteeseen toimii. Pelilooppi updatettu

--- lentoasemat.py
+++ lentoasemat.py
@@ -2,11 +2,12 @@ import random
 import mysql.connector
 from geopy import distance
 class lentokenttä:
-    def __init__(self, nimi, longitude, latitude, id = "EFHK"):
+    def __init__(self, nimi, longitude, latitude, municipality, id = "EFHK"):
         self.nimi = nimi
         self.id = id
         self.longitude = longitude
         self.latitude = latitude
+        self.municipality = municipality
 
 def kohteet():
     yhteys = mysql.connector.connect(
@@ -18,7 +19,7 @@ def kohteet():
         autocommit=True
     )
 
-    sql = "SELECT airport.name, longitude_deg, latitude_deg, airport.ident FROM airport"
+    sql = "SELECT airport.name, longitude_deg, latitude_deg, airport.ident, municipality FROM airport"
     sql += " inner join country on country.iso_country = airport.iso_country where country.name =" + "'Finland'" + " AND type = 'medium_airport'"
     sql += " order by latitude_deg"
     sql += " LIMIT 26"
@@ -27,7 +28,7 @@ def kohteet():
     kursori.execute(sql)
     tulos = kursori.fetchall()
     for rivi in tulos:
-        lentoasema = lentokenttä(rivi[0], rivi[1], rivi[2], rivi[3])
+        lentoasema = lentokenttä(rivi[0], rivi[1], rivi[2], rivi[4], rivi[3])
         lentokentta_lista.update({rivi[3]: lentoasema})
     return lentokentta_lista
 
---
+++ peli.py
@@ -0,0 +1,83 @@
+
+#Peli alkaa tästä. Pelilooppiin siirrytään kun pelaaja on valmis pelaamaan.
+import random
+import lentoasemat
+import etäisyys
+import siirryKohteeseen
+class Peli:
+    peliLoppu = 0
+    rahat = 500
+    polttoaine = 100
+    ilmastopisteet = 100
+    kilometrit = 0
+
+    pelaajanNimi = input(("Tervetuloa pelaamaan Läpi Suomen Maan! Syötä nimi: "))
+
+    print("Hei, " + pelaajanNimi)
+
+    lentoasema_lista = lentoasemat.kohteet()
+
+    nykyinenSijainti = lentoasema_lista["EFMA"]
+
+    mahdollisetKohteet = []
+
+    while peliLoppu == 0:
+        if nykyinenSijainti.id == "EFRO":
+            peliLoppu == 1
+            print("Voitit pelin, " + pelaajanNimi +  "!")
+            print("__  _")
+            print("\ `/ |")
+            print(" \__`!")
+            print(" / ,' `-.__________________")
+            print("'-'\_____                ◻ `-.")
+            print("   <____()-=O=O=O=O=O=[]====--)")
+            print("     `.___ ,-----,_______...-'")
+            print("          /    .'")
+            print("         /   .'")
+            print("        /  .'")
+            print("        `-'")
+            print("Ilmastopisteet: " + str(ilmastopisteet))
+            print("Kuljetut kilometrit: " + str(round(kilometrit, 1)))
+            break
+
+        if rahat <= 0:
+            print("GG")
+            break
+
+        print("Rahamäärä: " + str(round(rahat)) + " euroa")
+        print("Ilmastopisteet: " + str(ilmastopisteet))
+        print("Polttoaineesi: " + str(polttoaine) + " litraa")
+        print("Sijaintisi: " + nykyinenSijainti.nimi)
+
+        valinta = int(input("Mitä haluat tehdä? 1) Liiku 2) Tankkaa 3) Kartta 4) Poistu pelistä "))
+
+        if valinta == 1:
+            polttoaine, nykyinenSijainti, kilometrit, lentoasema_lista = siirryKohteeseen.valinnat(polttoaine, kilometrit, nykyinenSijainti, lentoasema_lista)
+
+            mahdollisetKohteet.clear() # Tyhjennetään lista.
+
+            ilmastopisteet -= 10
+            print("Kohteesi: " + nykyinenSijainti.nimi)
+
+            if polttoaine <= 0:
+                print("Polttoaine loppui ja koneesi tippui. Hävisit pelin.")
+                exit()
+
+        elif valinta == 2:
+            if rahat >= 100 - polttoaine:
+                määrä = (100 - polttoaine)
+                rahat -= 100 - polttoaine
+                round(rahat)
+                polttoaine = 100
+                ilmastopisteet -= 10
+                print("Tankkasit " + str(määrä) + " litraa." + " Menetit 10 ilmastopistettä.")
+
+        elif valinta == 3:
+            print("Avaa kartan.")
+
+        elif valinta == 4:
+            print("Hei hei!")
+            peliLoppu = 1
+
+        else:
+            print("Virheellinen syöte!")
