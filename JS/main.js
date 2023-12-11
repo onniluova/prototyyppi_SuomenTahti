f5e1d69af9ca43667eb7dc43bb7178f0f033769d
Jannassl <Jan.nassling@metropolia.fi>
2023-12-11 11:16:34 +0200

    json file

--- JS/main.js
+++ JS/main.js
@@ -4,7 +4,7 @@ L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
   maxZoom: 20,
   subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
 }).addTo(map);
-map.setView([60, 24], 7);
+map.setView([63, 24], 5.5);
 
 
 async function getData(url) {
@@ -16,10 +16,13 @@ async function getData(url) {
 
 async function gameSetup() {
     try {
-        const gameData = await getData('/JS/airports.json');
+        const gameData = await getData('http://127.0.0.1:5000/luoPeli');
+
+        console.log(gameData)
     }catch (error) {
         console.log(error)
     }
 }
+
 
 gameSetup()
